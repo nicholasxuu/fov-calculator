5c58ee627ad7d94d02ae5a0ca1822b1c0bf5b791
Shengfeng Xu <shengfeng.xu@hotmail.com>
2024-03-30 14:57:34 +0800

    remove base path for subdomain deploy

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: "export",
-  basePath: "/fov-calculator",
-  assetPrefix: "/fov-calculator",
+  // basePath: "/fov-calculator",
+  // assetPrefix: "/fov-calculator",
 };
 
 module.exports = nextConfig;
